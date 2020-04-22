53ec2a944b85fe2b6df9280f99342afddf44e016
joekhoa <pdkhoa01.sdh19@hcmut.edu.vn>
2020-04-22 17:31:02 +0700

    test browser api/recipe/ingredients/

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,78 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+from recipe.serializers import TagSerializer
+
+TAGS_URL = reverse('recipe:tag-list')
+
+class  PulicTagsApiTest(TestCase):       #  for all user
+    """Test the publicly available tags API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login is required for retrieving tags"""
+        res = self.client.get(TAGS_URL)
+
+class  PrivateTagsApiTest(TestCase) : # for a specific user
+    """Test the authorized user tags API"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'test_1@mail.com',
+            'password123'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving tags"""
+        Tag.objects.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=self.user, name='Dessert')
+
+        res = self.client.get(TAGS_URL)
+
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        # print('serializer', serializer)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tag_limited_to_user(self):
+        """Test that tags returned are for the authenticated user"""    # only specific user
+        user2 = get_user_model().objects.create_user(
+            'other@mail.com',
+            'testpass'
+        )
+        Tag.objects.create(user=user2, name='Fruity')
+        tag = Tag.objects.create(user=self.user, name='Comfort Food')
+
+        res = self.client.get(TAGS_URL)
+        # print(res)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)  #   ???
+        self.assertEqual(res.data[0]['name'], tag.name)  # ???
+
+    def test_create_tag_successful(self):
+        """Test create a new tag"""
+        payload = {'name':'Test tag'}
+        self.client.post(TAGS_URL, payload)
+
+        exist = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exist)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name':''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
